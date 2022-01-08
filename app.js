b7bce146cc304ae78f3a6469045797c0d57b680b
Minyoung-Jang <myimp9721@gmail.com>
2022-01-08 20:46:17 +0900

    [UPDATE] document 클릭 이벤트 구현

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hi");
+
+const title = document.querySelector("div.hello:first-child h1");
+
+function clickFunction(){
+    title.style.color = "blue";
+    console.log("h1 is clicked!");
+}
+
+title.addEventListener("click", clickFunction);
