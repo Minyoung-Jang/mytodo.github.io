86a75a683f6324d5e94142af12aff1a39ce8e299
Minyoung-Jang <myimp9721@gmail.com>
2022-01-13 22:30:29 +0900

    [UPDATE] event 발생

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function clickFunction() {
-    const clickClass = 'active'
-    if(h1.className === clickClass){
-        h1.className = '';
-    }else{
-        h1.className = clickClass;
-    }
+function handleMouseEnter(){
+    h1.innerHTML = "The mouse is enter!";
 }
 
-h1.addEventListener("click", clickFunction);
+function handleMouseGone(){
+    h1.innerHTML = "The mouse is gone!";
+}
+
+function handleWindowResize(){
+    h1.innerHTML = "You just resized!";
+}
+
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseGone);
+
+
+window.addEventListener("resize", handleWindowResize);
